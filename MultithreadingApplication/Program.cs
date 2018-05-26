d9ad2918b25bc7aff44aeff64f704ebd07e67f8f
mahesh.18jonna <mahesh.18jonna@gmail.com>
2018-05-26 17:30:40 -0400

    A thread is defined as the execution path of a program. Each thread defines a unique flow of control.

---
+++ MultithreadingApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingApplication
+{
+    class MainThreadProgram
+    {
+        static void Main(string[] args)
+        {
+            Thread th = Thread.CurrentThread;
+            th.Name = "MainThread";
+
+            Console.WriteLine("This is {0}", th.Name);
+            Console.ReadKey();
+        }
+    }
+}
